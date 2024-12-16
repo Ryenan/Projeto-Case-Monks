fefd1471ffc2c89d42d37881b5a745c76e12669d
Ryenan <ryenanaragaorodrigues@gmail.com>
2024-12-15 22:49:48 -0300

    99.99%

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const num1 = document.getElementById('num1');
+const num2 = document.getElementById('num2');
+
+const inputResult = document.getElementById('result');
+const verifyButton = document.getElementById('verifyButton');
+
+let correctSum;
+
+function generateRandomNumbers() {
+    const number1 = Math.floor(Math.random() * 900) + 100;
+    const number2 = Math.floor(Math.random() * 900) + 100;
+
+    num1.textContent = number1;
+    num2.textContent = number2;
+    correctSum = number1 + number2;
+}
+
+function verifyInput() {
+    const userResult = parseInt(inputResult.value);
+    if (userResult === correctSum) {
+        verifyButton.disabled = false;
+        verifyButton.classList.add('enabled');
+    } else {
+        verifyButton.disabled = true;
+        verifyButton.classList.remove('enabled');
+    }
+}
+
+    inputResult.addEventListener('input', verifyInput)
+    window.onload = generateRandomNumbers;
+
+
 function animar(){
     const buttaoanimar = document.getElementById('btnMenu');
     buttaoanimar.classList.toggle('ativaranimacao');
@@ -21,3 +53,76 @@ function menuShow(){
     }
 }
 
+function redirectToMonks() {
+    window.open('https://www.monks.com/pt', '_blank');
+}
+
+document.getElementById('imageLogo').addEventListener('click', redirectToMonks);
+document.getElementById('logoTextMobile').addEventListener('click', redirectToMonks);
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // var voltarParaTop = document.getElementById("voltarparaotop");
+
+    var sec1 = document.querySelector(".sec1");
+    var sec2 = document.querySelector(".sec2");
+    var sec3 = document.querySelector(".sec3");
+    var sec4 = document.querySelector(".sec4");
+    var sec5 = document.querySelector(".sec5");
+    var sec6 = document.querySelector(".form");
+    var sec6img = document.querySelector(".sec6Img");
+
+    sec1.classList.add('show');
+
+    window.addEventListener("scroll", function() {
+        
+        var alturaPagina = document.documentElement.scrollHeight - window.innerHeight;
+        var porcentagemRolagem = (window.scrollY / alturaPagina) * 100;
+        
+        // if (porcentagemRolagem > 35) { 
+        //     voltarparaotop.classList.add("show");
+        // } else {
+        //     voltarparaotop.classList.remove("show");
+        // }
+        
+        if (porcentagemRolagem > 8) {
+            sec2.classList.add("show");
+        } else {
+            sec2.classList.remove("show");      
+        }
+
+        if (porcentagemRolagem > 38) {
+            sec3.classList.add("show");
+        } else {
+            sec3.classList.remove("show");      
+        }
+
+        if (porcentagemRolagem > 52) {
+            sec4.classList.add("show");
+        } else {
+            sec4.classList.remove("show");      
+        }
+
+        if (porcentagemRolagem > 60) {
+            sec5.classList.add("show");
+        } else {
+            sec5.classList.remove("show");      
+        }
+
+        if (porcentagemRolagem > 77) {
+            sec6.classList.add("show");
+            sec6img.classList.add("show");
+        } else {
+            sec6.classList.remove("show");      
+            sec6img.classList.remove("show");      
+        }
+    })
+
+    // voltarparaotop.addEventListener("click", function() {
+    //     window.scrollTo({
+    //         top: 0,
+    //         behavior: "smooth" 
+    //     });
+    // });
+
+});
